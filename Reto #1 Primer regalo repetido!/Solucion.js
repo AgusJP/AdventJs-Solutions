4e6b29c62b264c0ad3a487c3c3e2ff712f920c9c
AgustinJzp <61051595+AgusJP@users.noreply.github.com>
2023-12-06 23:05:09 +0100

    Update Solucion.js

--- Reto #1 Primer regalo repetido!/Solucion.js
+++ Reto #1 Primer regalo repetido!/Solucion.js
@@ -1,12 +1,8 @@
 function findFirstRepeated(gifts) {
   const auxObj = {}
-
   for (let i = 0; i < gifts.length; i++) {
-    if (!auxObj.hasOwnProperty(gifts[i])) {
-      auxObj[gifts[i]] = i
-    } else {
-      return gifts[i]
-    }
+    if (auxObj.hasOwnProperty(gifts[i])) return gifts[i]  
+    auxObj[gifts[i]] = i
   }
   return -1
 }
